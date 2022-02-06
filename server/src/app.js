09153a9bd9ebdef61866695d485b035008ae087c
gugohh <oohhgug@gmail.com>
2022-02-06 22:09:00 +0100

    Server configuration and the server is runing successfully on port ****

---
+++ server/src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express(); 
+
+app.set("port", process.env.PORT || 4000); 
+
+app.use(cors()); 
+app.use(express.json()); 
+
+
+// app.use("/api/usuarios", require("./routes/usuario"));
+
+module.exports = app;
