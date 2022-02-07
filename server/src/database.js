f175b220833305255f808c292cbe2a8c2ca312fd
gugohh <oohhgug@gmail.com>
2022-02-07 16:21:03 +0100

    conection with mongooose

---
+++ server/src/database.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const URI = process.env.MONGODB_URI
+  ? process.env.MONGODB_URI
+  : "mongodb://localhost/dbtest";
+
+mongoose.connect(URI); 
+
+const connection = mongoose.connection; 
+
+connection.once("open", () => {
+  console.log("The database has been connected successfully: ", URI);
+});
