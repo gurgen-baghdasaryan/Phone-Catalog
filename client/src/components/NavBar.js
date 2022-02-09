96d12a3834d63f9f6fe4725d67de21c6e5a64ef0
gugohh <oohhgug@gmail.com>
2022-02-09 15:19:24 +0100

    working on Nav Bar component with bootstrap

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-    return (
-        <div>
-            
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          Phone Market
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                New Collection
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/CreateProduct">
+                Create Product
+              </Link>
+            </li>
+          </ul>
         </div>
-    )
-}
+      </div>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
