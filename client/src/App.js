d71863f737214c8bf3b9a92f42463dda9e787f70
gugohh <oohhgug@gmail.com>
2022-02-10 16:04:14 +0100

    added Product component to get mor details of our product

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,23 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
-import NavBar from './components/NavBar'
-import CreateProduct from './components/CreateProduct'
-import ProductList from './components/ProductList'
-
+import NavBar from "./components/NavBar";
+import CreateProduct from "./components/CreateProduct";
+import ProductList from "./components/ProductList";
+import Product from "./components/Product";
 
 function App() {
   return (
     <div>
-    <NavBar />
-    <div className="container p-4">
-      <Routes>
-        <Route exact path="/" element={<ProductList />} />
-        <Route exact path="/CreateProduct" element={<CreateProduct />} />
-        <Route exact path="/edit/:id" element={<CreateProduct />} />
-      </Routes>
-    </div>
+      <NavBar />
+      <div className="container p-4">
+        <Routes>
+          <Route exact path="/" element={<ProductList />} />
+          <Route exact path="/CreateProduct" element={<CreateProduct />} />
+          <Route exact path="/edit/:id" element={<CreateProduct />} />
+          <Route exact path="/product/:id" component={<Product />} />
+        </Routes>
+      </div>
     </div>
   );
 }
