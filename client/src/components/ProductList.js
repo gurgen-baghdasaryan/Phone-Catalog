16ed09a9ef91d1d59677284daa81f12a5013ba33
gugohh <oohhgug@gmail.com>
2022-02-10 16:04:50 +0100

    added Product component to get mor details of our product

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 
 const ProductList = (props) => {
-  const [list, setList] = useState([]);
+  const [list, setList] = useState({});
 
   useEffect(() => {
     const getProducts = async () => {
