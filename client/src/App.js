f77ff40334120f7cb1f60e975595dcacc6464add
gugohh <oohhgug@gmail.com>
2022-02-11 22:46:06 +0100

    added build folder

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@
 import { Routes, Route } from "react-router-dom";
 
 // Importing our components
+// Importing our components
 import "./App.css";
 import NavBar from "./components/NavBar";
 import CreateProduct from "./components/CreateProduct";
