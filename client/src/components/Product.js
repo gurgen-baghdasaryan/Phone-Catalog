3ff255cd57dce4cbe935bbed0266b411fc3be1b5
gugohh <oohhgug@gmail.com>
2022-02-12 11:30:53 +0100

    deploy

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -6,6 +6,7 @@ const Product = (props) => {
   // State where the product is stored
   const [product, setproduct] = useState({});
   // State where the loading is stored
+  // State where the loading is stored
   const [loading, setLoading] = useState(false);
 
   // Id of product
