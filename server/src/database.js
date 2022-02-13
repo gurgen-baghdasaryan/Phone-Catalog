3d009d94756b16818b9a32c65533996a6d7c2d9f
gugohh <oohhgug@gmail.com>
2022-02-13 12:32:35 +0100

    deploy

---
+++ server/src/database.js
@@ -0,0 +1,19 @@
+// Connection string to be able to connect our server with our database
+require("dotenv").config();
+
+
+const mongoose = require("mongoose");
+
+const URI = process.env.MONGODB_URI
+  ? process.env.MONGODB_URI
+  : "mongodb://localhost/dbtest";
+
+// It connects through the URI which is the variable we are connecting to the information we store in our environment variable.
+mongoose.connect(URI);
+
+// With this we are telling the mongoose to connect
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("The database has been connected successfully: ", URI);
+});
