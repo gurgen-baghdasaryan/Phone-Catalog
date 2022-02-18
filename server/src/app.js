fe0b66241ec464b96374e24776db29d04d62b799
gugohh <oohhgug@gmail.com>
2022-02-18 15:58:07 +0100

    make it better

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 // Server settings
+require("dotenv").config(); // 
 const express = require("express");
 const cors = require("cors");
 const connectionDB = require('./config/db');//conection to BBDD with Mongoose
